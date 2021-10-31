3a1f98abc7321eab6b4c894d9c7d588a9b9f34b5
Hari31416 <harikeshkumar0926@gmail.com>
2021-10-31 15:59:12 +0530

    Minor Changes

--- Machine Learning by Andrew Ng/Assignment/ex1-ex8-matlab/ex2/plotData.m
+++ Machine Learning by Andrew Ng/Assignment/ex1-ex8-matlab/ex2/plotData.m
@@ -4,8 +4,12 @@ function plotData(X, y)
 %   and o for the negative examples. X is assumed to be a Mx2 matrix.
 
 % Create New Figure
-figure; hold on;
-
+one = find(y==1);
+zero = find(y==0);
+figure;
+plot(X(one, 1), X(one, 2), 'k+','LineWidth', 2, 'MarkerSize', 7);
+hold on;
+plot(X(zero, 1), X(zero, 2), 'ko', 'MarkerFaceColor', 'y','MarkerSize', 7);
 % ====================== YOUR CODE HERE ======================
 % Instructions: Plot the positive and negative examples on a
 %               2D plot, using the option 'k+' for the positive
