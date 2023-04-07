973b56a16dae97f2e24ca754065bbc1a5ed2ab86
Harikesh Kushwaha <harikeshkumar0926@gmail.com>
2023-04-07 11:49:18 +0530

    Modified the model.py file

--- Codes/pytorch/model.py
+++ Codes/pytorch/model.py
@@ -53,3 +53,35 @@ class Conv1(nn.Module):
         x = self.block_2(x)
         x = self.classifier(x)
         return x
+        
+class TwoInputs(nn.Module):
+    def __init__(self, neurons=10, in1=1, in2=1):
+        super(TwoInputs, self).__init__()
+        self.neurons = neurons
+        self.in1 = in1
+        self.in2 = in2
+        self.b1 = self._branch_one()
+        self.b2 = self._branch_two()
+
+    def _branch_one(self):
+        b1 = nn.Sequential(
+            nn.Linear(self.in1, self.neurons),
+            nn.ReLU(),
+            nn.Linear(self.neurons, self.neurons),
+        )
+        return b1
+    
+    def _branch_two(self):
+        b2 = nn.Sequential(
+            nn.Linear(self.in2, self.neurons),
+            nn.ReLU(),
+            nn.Linear(self.neurons, self.neurons),
+        )
+        return b2
+        
+    def forward(self, x1, x2):
+        x1 = self.b1(x1)
+        x2 = self.b2(x2)
+        x = x1*x2
+        x = torch.sum(x, dim=1).unsqueeze(1)
+        return x
